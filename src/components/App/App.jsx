4c6c700a6f4b4b0fd1f45d39d5db5fef8c804140
Anna Taranchuk <taranchuk4@yahoo.com>
2024-05-08 17:25:45 +0300

    Update App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,21 @@
 import css from "./App.module.css";
 import Profile from "../Profile/Profile";
+import userData from "../Profile/userData.json";
 import FriendList from "../FriendList/FriendList";
+import friendData from "../FriendList/friends.json";
 import TransactionHistory from "../TransactionHistory/TransactionHistory";
 
 export default function App() {
   return (
     <>
-      <Profile />
-      <FriendList />
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friendData} />
       <TransactionHistory />
     </>
   );
