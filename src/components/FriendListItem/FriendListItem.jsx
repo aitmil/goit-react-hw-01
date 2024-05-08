12fab648ecd2eee77fa575b81a8f55944f076be1
Anna Taranchuk <taranchuk4@yahoo.com>
2024-05-08 17:50:59 +0300

    Update task 2

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,11 +1,11 @@
 import css from "./FriendListItem.module.css";
 
-export default function FriendListItem({ avatar, name, isOnline }) {
+export default function FriendListItem({ friend: { avatar, name, isOnline } }) {
   return (
     <div>
       <img src={avatar} alt="Avatar" width="48" />
       <p>{name}</p>
-      <p>{isOnline}</p>
+      <p>{isOnline ? "Online" : "Offline"}</p>
     </div>
   );
 }
