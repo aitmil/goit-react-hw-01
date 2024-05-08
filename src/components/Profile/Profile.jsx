44edabc5563d8280d7d641a19dc8b938ddc310f9
Anna Taranchuk <taranchuk4@yahoo.com>
2024-05-08 17:26:10 +0300

    Update task 1

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,10 +1,26 @@
 import css from "./Profile.module.css";
-import data from "./userData.json";
 
-export default function Profile() {
+export default function Profile({ name, tag, location, image, stats }) {
   return (
-    <>
-      <div></div>
-    </>
+    <div>
+      <div>
+        <img src={image} alt="User avatar" />
+        <p>{name}</p>
+        <p>{tag}</p>
+        <p>{location}</p>
+      </div>
+
+      <ul>
+        <li>
+          <span>Followers</span> <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span> <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span> <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
   );
 }
